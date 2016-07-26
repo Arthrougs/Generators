7d382abe719dfb73f581a11ccff85c0a138445eb
Casselman <cac03459@academia.sjfc.edu>
2016-07-26 12:24:08 -0400

    Basic generator constructor

--- GeneratorPlugin/src/org/lightningarc/www/Generator.java
+++ GeneratorPlugin/src/org/lightningarc/www/Generator.java
@@ -1,5 +1,19 @@
 package org.lightningarc.www;
 
-public class Generator {
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
+public class Generator {
+	
+	private Material material;
+	private int level;
+	private Player owner;
+	
+	public Generator(Material mat, int lvl, Player own){
+		this.material = mat;
+		this.level = lvl;
+		this.owner = own;
+	}
+	
+	
 }
